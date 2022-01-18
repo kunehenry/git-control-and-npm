dd1ba2ace617b5d9dff651cbc4b6d84f3d8f9bf1
Dat Nguyen <dtng19@gmail.com>
2022-01-18 10:02:08 -0600

    update files

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
+console.log("answer:", _.without(array, 3));
+//don't see anyhing, need to put in bundle.js: browserify script.js > bundle.js
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
